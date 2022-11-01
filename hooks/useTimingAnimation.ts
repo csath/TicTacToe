da84e07757614a4142665d6b4e6be2f3a3e5aa33
Chanaka Athurugiriya <chanakaathurugiriya@gmail.com>
2022-11-01 23:16:25 +0530

    create useTimingAnimation hook to handle animation

---
+++ hooks/useTimingAnimation.ts
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+import { Animated } from 'react-native';
+
+export default function useTimingAnimation(duration: number = 1000): { timingAnim: Animated.Value, restart: () => void } {
+  const timingAnim = useRef(new Animated.Value(0)).current;
+
+  const animate = () => {
+    Animated.timing(timingAnim, {
+      toValue: 1,
+      duration,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const restart = () => {
+    timingAnim.setValue(0)
+    animate()
+  }
+
+  useEffect(() => {
+    animate();
+  }, []);
+
+  return { timingAnim, restart };
+}
