9e2561f9bbb7f1a5f72be1905e063c330e41d30f
Chanaka Athurugiriya <chanakaathurugiriya@gmail.com>
2022-11-02 01:28:38 +0530

    fix styling

--- components/Cell/styles.ts
+++ components/Cell/styles.ts
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
-import Layout from '../../constants/Layout';
+import { StyleSheet } from "react-native";
+import Layout from "../../constants/Layout";
 
 const styles = StyleSheet.create({
   container: {
-    height: (Layout.window.width - 60) / 3,
-    width: (Layout.window.width - 60) / 3,
+    height: (Layout.window.width - Math.max(60, Layout.window.width / 6)) / 3,
+    width: (Layout.window.width - Math.max(60, Layout.window.width / 6)) / 3,
     justifyContent: "center",
     alignItems: "center",
   },
